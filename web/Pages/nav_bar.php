883e999decacfd9d43c6f6fc3b15fdfcec511b7b
Nefi Aguilar <solemnefi153@gmail.com>
2021-01-06 20:36:24 -0700

    Refactor links

--- web/Pages/nav_bar.php
+++ web/Pages/nav_bar.php
@@ -1,6 +1,6 @@
 <nav>
     <ul id='nav_bar'>
-        <li><a href='/index.php'>Home</a></li>
+        <li><a href='/'>Home</a></li>
         <li><a href="/Pages/assignments.php">Assignments</a></li>
         <li><a href="/Pages/portfolio.php">Porfolio</a></li>
         <li><a href="/Pages/contact.php">Contact</a></li>
