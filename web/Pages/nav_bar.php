381fde193390463af7409aa819d30dddc208fdc1
Nefi Aguilar <solemnefi153@gmail.com>
2021-07-20 15:10:22 -0600

    remove contact link for development

--- web/Pages/nav_bar.php
+++ web/Pages/nav_bar.php
@@ -4,6 +4,6 @@
         <!-- <li><a href="/Pages/assignments.php">Weekly readings</a></li> -->
         <li><a href="https://solemnefi153.github.io/WDD330/">Weekly readings</a></li> 
         <li><a href="https://tourmeapp.herokuapp.com/">Semester Project</a></li>
-        <li><a href="/Pages/contact.php">Contact</a></li>
+        <!-- <li><a href="/Pages/contact.php">Contact</a></li> -->
     </ul>
 </nav>
